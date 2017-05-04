551ee1d307b8656b3d999c0e51094be5eca179dd
Scott Davis <me@sdavis.info>
2017-05-04 11:05:13 -0400

    add new fish fuctions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,26 @@
 source ~/.asdf/asdf.fish
 eval (direnv hook fish)
+
+# nice light cyan color instead of dark blue
+set -gx LSCOLORS gxfxcxdxbxegedabagacad
+
+function ls --description 'List contents of directory'
+  command ls -lhFG $argv
+end
+
+function df --description 'Displays disk free space'
+  command df -H $argv
+end
+
+function grep --description 'Colorful grep that ignores binary file and outputs line number'
+  command grep --color=always -I $argv
+end
+
+function gf --description 'Do a git fetch'
+  command git fetch
+end
+
+function gdeletemergedcurrent --description 'Delete all local branches that is already merged to current branch (exludes master)'
+  command git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
+  command git remote prune origin
+end
