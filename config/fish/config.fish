15630d795e95103bf6f5dae035fd6fc707500e4f
Scott Davis <me@sdavis.info>
2025-04-01 00:11:43 +0000

    fish changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,137 +1,50 @@
+
+source $HOME/.config/fish/functions/my_functions.fish
+
 if test -e ~/.env
   source ~/.env
 end
-if test -e ~/.asdf/asdf.fish
-  echo "loading asdf from local"
-  source ~/.asdf/asdf.fish
-end
-if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
-end
 
-if test -e /Applications/Postgres.app/Contents/Versions/latest/bin/pg_config
-  fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
 
-if test -e /Users/sdavis/opt/miniconda3/bin/conda
-  fish_add_path /Users/sdavis/opt/miniconda3/bin
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
 end
 
-function kj
-  for x in (jobs -p); kill -9 $x; end
-end
+set --erase _asdf_shims
 
-function reshim-golang
-  command asdf reshim golang && export GOROOT=(asdf where golang)"/go/"
-end
-
-function gotest
-  command go test ./...
-end
-
-function read_confirm
-  while true
-    read -l -P 'Do you want to continue? [y/N] ' confirm
-
-    switch $confirm
-      case Y y
-        return 0
-      case '' N n
-        return 1
-    end
+if is_osx
+  if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
   end
-end
-
-function nv
-  if contains (jobs -c) "nvim"
-    set the_jobs (jobs | grep nvim | awk -F '\t' '{print $2}')
-    if test (count $the_jobs) -gt 1
-      echo "Notice: Multiple jobs running"
-      for j in (jobs | grep nvim); echo $j; end
-      echo "Picking $the_jobs[1] by default"
-      if read_confirm
-        fg $the_jobs[1]
-      end
-    else
-      fg $the_jobs[1]
-    end
-  else
-    command nvim $argv
+  if test -e /Applications/Postgres.app/Contents/Versions/latest/bin/pg_config
+    fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
   end
+
+  if test -e /Applications/Postgres.app/Contents/Versions/14/bin/clusterdb
+    fish_add_path /Applications/Postgres.app/Contents/Versions/14/bin
+  end
+
+  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+  fish_add_path /opt/homebrew/opt/curl/bin
+  fish_add_path /opt/homebrew/bin
 end
 
-if test -e /Applications/Postgres.app/Contents/Versions/14/bin/clusterdb
-  fish_add_path /Applications/Postgres.app/Contents/Versions/14/bin
-end
-
-fish_add_path /usr/local/bin
-fish_add_path /opt/homebrew/opt/curl/bin
-fish_add_path /opt/homebrew/bin
-fish_add_path /Users/sdavis/go/bin
 # nice light cyan color instead of dark blue
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
-function ls --description 'List contents of directory'
-  command ls -lhFG $argv
-end
+fish_add_path ./bin
+fish_add_path /usr/local/bin
+fish_add_path /Users/sdavis/go/bin
 
-function df --description 'Displays disk free space'
-  command df -H $argv
-end
-
-function grep --description 'Colorful grep that ignores binary file and outputs line number'
-  command grep --color=always -I $argv
-end
-
-function gg --description 'git grep with color'
-  command git grep -n --color $argv
-end
-
-function git-replace --description "replace all in repo"
-  set replace $argv[1];
-  set to_replace $argv[2];
-  switch (uname)
-    case Linux
-      command git grep -l $replace | xargs sed -i '' "s/$replace/$to_replace/g"
-    case Darwin
-      command git grep -l $replace | xargs sed -i '' -e "s/$replace/$to_replace/g"
-  end
-end
-
-function gf --description 'Do a git fetch'
-  command git fetch
-end
-
-function gs --description 'Do a git status'
-  command git status
-end
-
-function gdeletemergedcurrent --description 'Delete all local branches that is already merged to current branch (exludes master)'
-  command git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
-  command git remote prune origin
-end
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-if test -e /opt/homebrew/bin/direnv
+if type -q direnv
   direnv hook fish | source;
 end
-set PATH $PATH ./bin
-set PATH $PATH /usr/local/bin
-set PATH $PATH /opt/homebrew/bin
-set PATH $PATH /usr/local/google-cloud-sdk/bin
-
-
-direnv hook fish | source;
-
-set PATH ./bin $PATH
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/sdavis/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/sdavis/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/sdavis/Downloads/google-cloud-sdk/path.fish.inc'; end
